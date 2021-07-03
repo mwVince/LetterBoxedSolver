341568bcc221d43dcd1688882e6846206e7e952f
mwVince <mw.vince.chang@gmail.com>
2021-07-02 22:19:10 -0400

    Added input validation

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Driver {
@@ -7,31 +8,115 @@ public class Driver {
     public static void main(String[] args) {
         WordTreeNode root = new WordTreeNode();
         WordTreeMaker treeMaker = new WordTreeMaker(root);
-        System.out.print("Which word list to used (1. Full Dictionary | 2. Most Frequent 10,000 Used Words): ");
-        treeMaker.makeTree(Integer.parseInt(scanner.nextLine()));
+
+        while (true) {
+            int wordListNumber;
+            System.out.print("Which word list to used (1. Full Dictionary | 2. Most Frequent 10,000 Used Words): ");
+            if (!scanner.hasNextInt()) {
+                System.out.println("Invalid input, please enter 1 or 2");
+                scanner.next();
+            }
+            else {
+                wordListNumber = scanner.nextInt();
+                if (wordListNumber != 1 && wordListNumber != 2) {
+                    System.out.println("Invalid input, please enter 1 or 2");
+                }
+                else {
+                    treeMaker.makeTree(wordListNumber);
+                    break;
+                }
+            }
+        }
 
         char[][] chars = makePuzzle();
-        System.out.print("Max number of words: ");
-        int maxWords = scanner.nextInt();
+
+        int maxWords;
+        while (true) {
+            int max;
+            System.out.print("\nMax number of words: ");
+            if (!scanner.hasNextInt()) {
+                System.out.println("Invalid input, please enter a positive integer");
+                scanner.next();
+            }
+            else {
+                max = scanner.nextInt();
+                if (max <= 0) {
+                    System.out.println("Invalid input, please enter a positive integer");
+                }
+                else {
+                    maxWords = max;
+                    break;
+                }
+            }
+        }
+
         PuzzleSolver puzzleSolver = new PuzzleSolver(chars[0], chars[1], chars[2], chars[3], root, maxWords);
-
         ArrayList<ArrayList<String>> solutions = puzzleSolver.solve();
-
-        System.out.println("Finished " + solutions.size() + " solutions found");
+        System.out.println("\nFinished " + solutions.size() + " solutions found");
     }
 
     static char[][] makePuzzle() {
-        System.out.println("Enter puzzle in form of xyz");
-        System.out.print("First chars: ");
-        char[] chars1 = makeChars(scanner.nextLine());
-        System.out.print("Second chars: ");
-        char[] chars2 = makeChars(scanner.nextLine());;
-        System.out.print("Third chars: ");
-        char[] chars3 = makeChars(scanner.nextLine());
-        System.out.print("Forth chars: ");
-        char[] chars4 = makeChars(scanner.nextLine());
+        HashSet<Character> puzzleChars = new HashSet<>();
+
+        System.out.println("\nEnter puzzle in form of xyz");
+
+        String chars;
+        do {
+            System.out.print("First chars: ");
+            chars = scanner.nextLine();
+        } while (!isInputCharsValid(puzzleChars, chars));
+        char[] chars1 = makeChars(chars);
+
+        chars = "";
+        do {
+            System.out.print("Second chars: ");
+            chars = scanner.nextLine();
+        } while (!isInputCharsValid(puzzleChars, chars));
+        char[] chars2 = makeChars(chars);
+
+        chars = "";
+        do {
+            System.out.print("Third chars: ");
+            chars = scanner.nextLine();
+        } while (!isInputCharsValid(puzzleChars, chars));
+        char[] chars3 = makeChars(chars);
+
+        chars = "";
+        do {
+            System.out.print("Forth chars: ");
+            chars = scanner.nextLine();
+        } while (!isInputCharsValid(puzzleChars, chars));
+        char[] chars4 = makeChars(chars);
 
         return new char[][]{chars1, chars2, chars3, chars4};
+    }
+
+    static boolean isInputCharsValid(HashSet<Character> puzzleChars, String inputChars) {
+        HashSet<Character> temp = new HashSet<>();
+
+        if (inputChars.length() != 3) {
+            System.out.println("Please enter 3 characters");
+            return false;
+        }
+
+        for (char c: inputChars.toCharArray()) {
+            if (!Character.isAlphabetic(c)) {
+                System.out.println(c + " is not an alphabet, please enter only alphabetic characters");
+                return false;
+            }
+            else if (puzzleChars.contains(c) || temp.contains(c)) {
+                System.out.println(c + " already in the puzzle, please enter non repeating characters");
+                return false;
+            }
+            else {
+                temp.add(c);
+            }
+        }
+
+        for (char c: temp) {
+            puzzleChars.add(c);
+        }
+        return true;
     }
 
     static char[] makeChars(String str) {
